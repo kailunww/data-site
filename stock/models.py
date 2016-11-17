803abac8b7849b698c97671953011dc62b6a8321
Alan Wong <kailunww@gmail.com>
2016-11-17 21:38:57 +0800

    init data

--- stock/models.py
+++ stock/models.py
@@ -16,3 +16,55 @@ class StockCode(models.Model):
     name_eng = models.CharField(max_length=100)
     lot_size = models.PositiveIntegerField(default=0)
 
+
+class Project:
+    IPO = 1
+    LONG = 2
+    SHORT = 3
+
+
+class StockIn(models.Model):
+    project_choices = [
+        (Project.IPO, "IPO"),
+        (Project.LONG, "LONG"),
+        (Project.SHORT, "SHORT"),
+
+    ]
+    code = models.ForeignKey(StockCode)
+    add_date = models.DateField()
+    broker = models.ForeignKey(Broker)
+    project = models.PositiveSmallIntegerField(choices=project_choices)
+    money = models.DecimalField(max_length=14, max_digits=4)
+    price = models.DecimalField(max_length=14, max_digits=4)
+    quantity = models.PositiveSmallIntegerField()
+    parent = models.ForeignKey("StockIn")
+
+
+class MoneyIn(models.Model):
+    stock = models.ForeignKey(StockIn)
+    add_date = models.DateField()
+    money = models.DecimalField(max_length=14, max_digits=4)
+    price = models.DecimalField(max_length=14, max_digits=4)
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Dividend(models.Model):
+    stock = models.ForeignKey(StockIn)
+    add_date = models.DateField()
+    money = models.DecimalField(max_length=14, max_digits=4)
+    price = models.DecimalField(max_length=14, max_digits=4)
+    quantity = models.PositiveSmallIntegerField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
